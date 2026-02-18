f228a84858e29fc3e245bfd6cd2deb077ab66763
Ubuntu <ubuntu@ip-172-31-86-131.ec2.internal>
2026-02-18 00:56:20 +0000

    Fix: añadir s3

--- pipelines/common-steps/deploy.sh
+++ pipelines/common-steps/deploy.sh
@@ -11,6 +11,7 @@ sam deploy --template-file .aws-sam/build/template.yaml \
 	    --config-env "${ENVIRONMENT}" \
 	      --stack-name "${STACK_NAME}" \
 	        --region "${AWS_REGION}" \
+		--resolve-s3 \
 		  --no-confirm-changeset \
 		    --force-upload \
 		      --no-fail-on-empty-changeset \
