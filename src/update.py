05e590def3a3600b614b581df8c618369484ae6d
Ubuntu <ubuntu@ip-172-31-86-131.ec2.internal>
2026-02-18 16:32:02 +0000

    fix update.py

--- src/update.py
+++ src/update.py
@@ -5,22 +5,21 @@ import todoList
 
 
 def update(event, context):
-        data = json.loads(event['body'])
+    data = json.loads(event['body'])
 
-            if 'text' not in data or 'checked' not in data:
-                        logging.error("Validation Failed")
-                                raise Exception("Couldn't update the todo item.")
+    if 'text' not in data or 'checked' not in data:
+        logging.error("Validation Failed")
+        raise Exception("Couldn't update the todo item.")
 
-                                result = todoList.update_item(
-                                                event['pathParameters']['id'],
-                                                        data['text'],
-                                                                data['checked']
-                                                                    )
+    result = todoList.update_item(
+        event['pathParameters']['id'],
+        data['text'],
+        data['checked']
+    )
 
-                                    response = {
-                                                    "statusCode": 200,
-                                                            "body": json.dumps(result, cls=decimalencoder.DecimalEncoder)
-                                                                }
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(result, cls=decimalencoder.DecimalEncoder)
+    }
 
-                                        return response
-
+    return response
